c74e7188773bec96736c8f73d2c4fed4f463a174
David Lemcoe Jr. <david@lemcoe.com>
2026-02-21 13:57:45 -0500

    More Windows error-fixes

--- windows/StripedPrinter/TrayApp.cs
+++ windows/StripedPrinter/TrayApp.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace StripedPrinter;
 
